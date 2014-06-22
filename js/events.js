f61745b6fa7ec47cf3ef314db90b2ee2c5134328
JordiSanchezMarin <jordisanchez00@gmail.com>
2014-06-22 16:00:00 +0200

    Transition 3D (compete)
    Transition in modal Contact

--- js/events.js
+++ js/events.js
@@ -1,11 +1,56 @@
 $(document).ready(function(){
-	$("#btn_contact_showcase").click(function(){
-		$(this).attr("disabled", "disabled");
-		$("#spinner").addClass("onetotwo");
-		setTimeout(function(){
-			$("#spinner").removeClass("onetotwo");
-			$("#face1").html("2");
+	btn_3D_Cick();
+	function btn_3D_Cick(){
+		$(".btn_3d").click(function(){
+			$(this).parent().find("button").addClass("btn_3d").unbind();
+			$(this).removeClass("btn_3d");
+			$("#spinner").addClass("turnRight");
+			if($(this).attr("id") == "btn_contact_general"){
+				$("#face2").html("General");
+				setTimeout(function(){
+					$("#spinner").removeClass("turnRight");
+					$("#face1").html("General");
+					btn_3D_Cick();
+				}, 2000);
+			}
+			else if($(this).attr("id") == "btn_contact_showcase"){
+				$("#face2").html("ShowCase");
+				setTimeout(function(){
+					$("#spinner").removeClass("turnRight");
+					$("#face1").html("ShowCase");
+					btn_3D_Cick();
+				}, 2000);
+			}
+			else if($(this).attr("id") == "btn_contact_demo"){
+				$("#face2").html("Send Demo");
+				setTimeout(function(){
+					$("#spinner").removeClass("turnRight");
+					$("#face1").html("Send Demo");
+					btn_3D_Cick();
+				}, 2000);
+			}
 			
+		});
+	}
+	/*$("#btn_contact_general").unbind();
+	$("#btn_contact_showcase").click(function(){
+		$(this).parent().find("button").bind("click");
+		$(this).unbind();
+		$("#spinner").addClass("turnRight");
+		$("#face2").html("ShowCase");
+		setTimeout(function(){
+			$("#spinner").removeClass("turnRight");
+			$("#face1").html("ShowCase");
 		}, 2000);
 	});
+	$("#btn_contact_general").click(function(){
+		$(this).parent().find("button").bind("click");
+		$(this).unbind();
+		$("#spinner").addClass("turnRight");
+		$("#face2").html("General");
+		setTimeout(function(){
+			$("#spinner").removeClass("turnRight");
+			$("#face1").html("General");
+		}, 2000);
+	});*/
 });
