51e773f213fab2ce43dd06e2bbabc506729d185e
JordiSanchezMarin <jordisanchez00@gmail.com>
2014-07-07 14:39:09 +0200

    Transition 3D Modal --> OK

--- js/events.js
+++ js/events.js
@@ -1,31 +1,33 @@
 $(document).ready(function(){
+	// exececute function to add a click envent on modal buttons
 	btn_3D_Cick();
+	//declare function wich add event to modal button
 	function btn_3D_Cick(){
 		$(".btn_3d").click(function(){
 			$(this).parent().find("button").addClass("btn_3d").unbind();
 			$(this).removeClass("btn_3d");
 			$("#spinner").addClass("turnRight");
 			if($(this).attr("id") == "btn_contact_general"){
-				$("#face2").html("General");
+				$("#face2").html(forms.generalForm());
 				setTimeout(function(){
 					$("#spinner").removeClass("turnRight");
-					$("#face1").html("General");
+					$("#face1").html(forms.generalForm());
 					btn_3D_Cick();
 				}, 1000);
 			}
 			else if($(this).attr("id") == "btn_contact_showcase"){
-				$("#face2").html("ShowCase");
+				$("#face2").html(forms.showcaseForm());
 				setTimeout(function(){
 					$("#spinner").removeClass("turnRight");
-					$("#face1").html("ShowCase");
+					$("#face1").html(forms.showcaseForm());
 					btn_3D_Cick();
 				}, 1000);
 			}
 			else if($(this).attr("id") == "btn_contact_demo"){
-				$("#face2").html("Send Demo");
+				$("#face2").html(forms.sendDemo());
 				setTimeout(function(){
 					$("#spinner").removeClass("turnRight");
-					$("#face1").html("Send Demo");
+					$("#face1").html(forms.sendDemo());
 					btn_3D_Cick();
 				}, 1000);
 			}
