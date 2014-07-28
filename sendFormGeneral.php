bb1739c1619dc7f8f81ca4e3c0f687c8d12cc1f2
JordiSanchezMarin <jordisanchez00@gmail.com>
2014-07-28 20:51:18 +0200

    Revert "Develop send mail contact"
    This reverts commit 4ffe78e53c07a36a75a5d97be0284d5898c7aed4.

--- sendFormGeneral.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$name = $_GET['name'];
-	$email = $_GET['email'];
-	$subject = $_GET['subject'];
-	$mesaje = $_GET['mesaje'];
-	mail("jordisanchez00@gmail.com", $subject, $mesaje, $email);
-?>
