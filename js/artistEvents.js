22be5ff9b13c654adad6bdcb7d589ce3c88569e2
JordiSanchezMarin <jordisanchez00@gmail.com>
2014-08-25 18:41:29 +0200

    image and artist description
    not finished

--- js/artistEvents.js
+++ js/artistEvents.js
@@ -16,6 +16,27 @@ $(document).ready(function(){
 				$(this).parent().append("<div id='infoArtist'></div>");
 				$("#infoArtist").append("<h1 style='margin: 0px'>" + artistName  + "</h1>");
 				$("#infoArtist").append("<img src='img/closeIcon.png' class='closeImage'/>");
+				if(artistName == "Gustavho Bacilo"){
+					$("#infoArtist").append(Artists.Gustavho);
+				}
+				else if(artistName == "Krusound" ){
+					$("#infoArtist").append(Artists.Krusound);
+				}
+				else if(artistName == "German"){
+					$("#infoArtist").append(Artists.German);					
+				}
+				else if(artistName == "Jandro Beat"){
+					$("#infoArtist").append(Artists.Jandro);					
+				}
+				else if(artistName == "Lexlay"){
+					$("#infoArtist").append(Artists.Lexlay);					
+				}
+				else if(artistName == "Cesar"){
+					$("#infoArtist").append(Artists.Cesar);					
+				}
+				else if(artistName == "Agatha Pher"){
+					$("#infoArtist").append(Artists.Agatha);					
+				}
 				$("#infoArtist").fadeIn("fast");
 				closeArtistClick();
 			});
